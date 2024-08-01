b9daccd2965abe8af4d910fd8599f1e9989d84f9
Alisher02 <alisherabdullaev02@gmail.com>
2024-08-01 23:04:21 +0500

    testng

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,19 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
 public class Main {
+
+    @Test
+    public void test1(){
+        System.out.println("test1");
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+
+
+
+
     }
 }
---
+++ src/main/java/org/example/Test2.java
@@ -0,0 +1,12 @@
+package org.example;
+
+
+import org.junit.jupiter.api.Test;
+
+public class Test2 {
+
+    @Test
+    public void test2(){
+        System.out.println("test2");
+    }
+}

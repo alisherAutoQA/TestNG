91218b942e8bf99cb23e0c27aaa99c2509fa5466
Alisher02 <alisherabdullaev02@gmail.com>
2024-08-02 21:13:48 +0500

    testngg

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,24 @@
 package org.example;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
 
-import org.junit.jupiter.api.Test;
 public class Main {
 
-    @Test
-    public void test1(){
-        System.out.println("test1");
+    @DataProvider(name="db")
+    public static Object[][] dbData (){
+        return new Object[][] {
+                {"SQL", new Integer(1)},
+                {"NoSQL", new Integer(2)}
+        };
+    }
+
+
+
+    @Parameters({"db"})
+    @Test(dataProvider = "db")
+    public void test1(String e1, int e2){
+        System.out.println("test1 " + e1 + " "+ e2);
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/org/example/test2.java
@@ -0,0 +1,11 @@
+package org.example;
+import org.testng.annotations.*;
+
+
+public class test2 {
+
+    @Test()
+    public void test3(String db){
+        System.out.println("test2");
+    }
+}
